d36629b56fa5e7692bd4102cf9137c204c8cdf29
Nik000111222 <nejcinnikson@gmail.com>
2024-05-22 21:11:54 +0200

    moja.igra main.py

--- main.py
+++ main.py
@@ -1,39 +1,34 @@
 import pygame
 from random import randrange as rnd
 
-WIDTH, HEIGHT = 1200, 800
+ŠIRINA, VIŠINA = 1200, 800
 fps = 60
-# paddle settings
-paddle_w = 330
-paddle_h = 35
-paddle_speed = 15
-paddle = pygame.Rect(WIDTH // 2 - paddle_w // 2, HEIGHT - paddle_h - 10, paddle_w, paddle_h)
-# ball settings
-ball_radius = 20
-ball_speed = 6
-ball_rect = int(ball_radius * 2 ** 0.5)
-ball = pygame.Rect(rnd(ball_rect, WIDTH - ball_rect), HEIGHT // 2, ball_rect, ball_rect)
+lopar_š = 330
+lopar_v = 35
+hitrost_loparja = 15
+lopar = pygame.Rect(ŠIRINA // 2 - lopar_š // 2, VIŠINA - lopar_v - 10, lopar_š, lopar_v)
+polmer_žogice = 20
+hitrost_žogice = 6
+pravokotnik_žogice = int(polmer_žogice * 2 ** 0.5)
+žogica = pygame.Rect(rnd(pravokotnik_žogice, ŠIRINA - pravokotnik_žogice), VIŠINA // 2, pravokotnik_žogice, pravokotnik_žogice)
 dx, dy = 1, -1
-# blocks settings
-block_list = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
-color_list = [(rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(10) for j in range(4)]
+seznam_blokov = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
+seznam_barv = [(rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(10) for j in range(4)]
 
 pygame.init()
-sc = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-# background image
-img = pygame.image.load('1.jpg').convert()
+zaslon = pygame.display.set_mode((ŠIRINA, VIŠINA))
+ura = pygame.time.Clock()
+slika = pygame.image.load('1.jpg').convert()
 
-
-def detect_collision(dx, dy, ball, rect):
+def zaznaj_trk(dx, dy, žogica, pravokotnik):
     if dx > 0:
-        delta_x = ball.right - rect.left
+        delta_x = žogica.right - pravokotnik.left
     else:
-        delta_x = rect.right - ball.left
+        delta_x = pravokotnik.right - žogica.left
     if dy > 0:
-        delta_y = ball.bottom - rect.top
+        delta_y = žogica.bottom - pravokotnik.top
     else:
-        delta_y = rect.bottom - ball.top
+        delta_y = pravokotnik.bottom - žogica.top
 
     if abs(delta_x - delta_y) < 10:
         dx, dy = -dx, -dy
@@ -43,55 +38,40 @@ def detect_collision(dx, dy, ball, rect):
         dx = -dx
     return dx, dy
 
-
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for dogodek in pygame.event.get():
+        if dogodek.type == pygame.QUIT:
             exit()
-    sc.blit(img, (0, 0))
-    # drawing world
-    [pygame.draw.rect(sc, color_list[color], block) for color, block in enumerate(block_list)]
-    pygame.draw.rect(sc, pygame.Color('darkorange'), paddle)
-    pygame.draw.circle(sc, pygame.Color('white'), ball.center, ball_radius)
-    # ball movement
-    ball.x += ball_speed * dx
-    ball.y += ball_speed * dy
-    # collision left right
-    if ball.centerx < ball_radius or ball.centerx > WIDTH - ball_radius:
+    zaslon.blit(slika, (0, 0))
+    [pygame.draw.rect(zaslon, seznam_barv[barva], blok) for barva, blok in enumerate(seznam_blokov)]
+    pygame.draw.rect(zaslon, pygame.Color('darkorange'), lopar)
+    pygame.draw.circle(zaslon, pygame.Color('white'), žogica.center, polmer_žogice)
+    žogica.x += hitrost_žogice * dx
+    žogica.y += hitrost_žogice * dy
+    if žogica.centerx < polmer_žogice or žogica.centerx > ŠIRINA - polmer_žogice:
         dx = -dx
-    # collision top
-    if ball.centery < ball_radius:
+    if žogica.centery < polmer_žogice:
         dy = -dy
-    # collision paddle
-    if ball.colliderect(paddle) and dy > 0:
-        dx, dy = detect_collision(dx, dy, ball, paddle)
-        # if dx > 0:
-        #     dx, dy = (-dx, -dy) if ball.centerx < paddle.centerx else (dx, -dy)
-        # else:
-        #     dx, dy = (-dx, -dy) if ball.centerx >= paddle.centerx else (dx, -dy)
-    # collision blocks
-    hit_index = ball.collidelist(block_list)
-    if hit_index != -1:
-        hit_rect = block_list.pop(hit_index)
-        hit_color = color_list.pop(hit_index)
-        dx, dy = detect_collision(dx, dy, ball, hit_rect)
-        # special effect
-        hit_rect.inflate_ip(ball.width * 3, ball.height * 3)
-        pygame.draw.rect(sc, hit_color, hit_rect)
+    if žogica.colliderect(lopar) and dy > 0:
+        dx, dy = zaznaj_trk(dx, dy, žogica, lopar)
+    zadeti_indeks = žogica.collidelist(seznam_blokov)
+    if zadeti_indeks != -1:
+        zadeti_pravokotnik = seznam_blokov.pop(zadeti_indeks)
+        zadeta_barva = seznam_barv.pop(zadeti_indeks)
+        dx, dy = zaznaj_trk(dx, dy, žogica, zadeti_pravokotnik)
+        zadeti_pravokotnik.inflate_ip(žogica.width * 3, žogica.height * 3)
+        pygame.draw.rect(zaslon, zadeta_barva, zadeti_pravokotnik)
         fps += 2
-    # win, game over
-    if ball.bottom > HEIGHT:
-        print('GAME OVER!')
+    if žogica.bottom > VIŠINA:
+        print('KONEC IGRE!')
         exit()
-    elif not len(block_list):
-        print('WIN!!!')
+    elif not len(seznam_blokov):
+        print('ZMAGA!!!')
         exit()
-    # control
-    key = pygame.key.get_pressed()
-    if key[pygame.K_LEFT] and paddle.left > 0:
-        paddle.left -= paddle_speed
-    if key[pygame.K_RIGHT] and paddle.right < WIDTH:
-        paddle.right += paddle_speed
-    # update screen
+    tipka = pygame.key.get_pressed()
+    if tipka[pygame.K_LEFT] and lopar.left > 0:
+        lopar.left -= hitrost_loparja
+    if tipka[pygame.K_RIGHT] and lopar.right < ŠIRINA:
+        lopar.right += hitrost_loparja
     pygame.display.flip()
-    clock.tick(fps)
+    ura.tick(fps)
